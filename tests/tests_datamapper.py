87b20ab5361473afbc7da43ada247fca7ab5b6d2
Antash <4nt45h@gmail.com>
2015-05-18 00:17:15 +0200

    preparing to develop datamapper class

---
+++ tests/tests_datamapper.py
@@ -0,0 +1,15 @@
+import unittest
+from code import datamapping as dm
+
+
+class TestDataMapper(unittest.TestCase):
+
+    def setUp(self):
+        test_object = dm.Importer
+        test_object.import_data("./tests/cvrp1.test")
+
+    def test_blablabla(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.run()
