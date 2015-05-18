ec21a1105ed63a34b12c49895b9a8e1369bd6690
Antash <4nt45h@gmail.com>
2015-05-18 23:24:46 +0200

    initial testing suite for datamapper class

--- tests/tests_datamapper.py
+++ tests/tests_datamapper.py
@@ -5,11 +5,11 @@ from code import datamapping as dm
 class TestDataMapper(unittest.TestCase):
 
     def setUp(self):
-        test_object = dm.Importer
+        test_object = dm.Importer()
         test_object.import_data("./tests/cvrp1.test")
 
     def test_blablabla(self):
         pass
 
 if __name__ == "__main__":
-    unittest.run()
+    unittest.main()
