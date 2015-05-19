6fc2cd7bd8ad83b7e52e805b3efd1994e35da409
Antash <4nt45h@gmail.com>
2015-05-19 13:31:13 +0200

    added base for baseobjects tests suite

---
+++ tests/tests_baseobjects.py
@@ -0,0 +1,41 @@
+import unittest
+from code import baseobjects as bo
+
+
+class TestNode(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        pass
+
+
+class TestVehicle(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        pass
+
+
+class TestRoute(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        pass
+
+
+class TestNetwork(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
