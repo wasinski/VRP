e99f977866e80cef0610f86ad1f4c851568196b4
Antash <4nt45h@gmail.com>
2015-06-02 22:26:23 +0200

    started developing BnB class

--- code/branchnbound.py
+++ code/branchnbound.py
@@ -1,2 +1,31 @@
 class BranchNBound(object):
-    pass
+
+    def __init__(self, instance):
+        self.partial_solutions = []
+        self.upper_bound = None
+        self.current_best = None
+
+    def bound(self):
+        pass
+
+    def branch(self):
+        pass
+
+    def run(self):
+        pass
+
+
+class BnBPartialSolution(object):
+
+    def __init__(self, instance):
+        self.solution = None
+        self.lower_bound = None
+        self.edges = {True: [], False: []}
+
+    def construct_route(self):
+        pass
+
+
+"""
+ok, więc na wstępie potrzebuję odpowiednich struktur z możliwością ich kopiowania:
+    instancja problemu rozszerzona"""
