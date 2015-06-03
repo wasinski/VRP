197729f6ffc4b43cddc68b317804d18bdcb55fc3
Antash <4nt45h@gmail.com>
2015-06-03 02:42:13 +0200

    added distance matrix converting method used by BnB algorithm, tested and working

--- code/branchnbound.py
+++ code/branchnbound.py
@@ -1,9 +1,47 @@
-class BranchNBound(object):
+import numpy as np
 
+
+class BranchNBound(object):
+    """ Branch and bound implementation
+
+        This algorithm uses m-TSP relaxation and BnB method proposed by Little et. al.
+        Additionally routes for every vehicle are constructed at end of every iteration
+            and solutions that wont be feasible are detected, and pruned.
+    """
     def __init__(self, instance):
         self.partial_solutions = []
         self.upper_bound = None
         self.current_best = None
+        self.times_branched = 0
+        self.initial_upper_bound = None
+
+    def run(self):
+        pass
+
+    def select_most_promising(self):
+        pass
+
+    def prune(self):
+        pass
+
+
+class BnBPartialSolution(object):
+
+    def __init__(self, partial_solution):
+        self.network = partial_solution.network
+        self.fleet = partial_solution.fleet
+        self.distance_matrix = partial_solution.distance_matrix
+        self.lower_bound = partial_solution.lower_bound
+        self.edges = partial_solution.edges
+
+    @classmethod
+    def init_from_instance(cls, instance):
+        network = instance.network
+        fleet = instance.fleet
+        distance_matrix = BnBPartialSolution.convert(instance.distance_matrix)
+        lower_bound = None
+        edges = {True: [], False: []}
+        return cls(network, fleet, distance_matrix, lower_bound, edges)
 
     def bound(self):
         pass
@@ -11,21 +49,58 @@ class BranchNBound(object):
     def branch(self):
         pass
 
-    def run(self):
-        pass
-
-
-class BnBPartialSolution(object):
-
-    def __init__(self, instance):
-        self.solution = None
-        self.lower_bound = None
-        self.edges = {True: [], False: []}
-
     def construct_route(self):
         pass
 
+    def update_solution(self):
+        pass
 
-"""
-ok, więc na wstępie potrzebuję odpowiednich struktur z możliwością ich kopiowania:
-    instancja problemu rozszerzona"""
+    def convert(matrix, fleet_size):
+        converted = []
+        # initialize matrix
+        for i in range(len(matrix) + fleet_size):
+            row = [float("inf")] * (len(matrix) + fleet_size)
+            converted.append(row)
+
+        # please remember that first $fleet_size rows are index = 1, and contain 'to depot' distance
+        # make first row an index row
+
+        for i in range(1, len(converted[0])):
+            if i <= fleet_size:
+                converted[0][i] = 1
+            else:
+                converted[0][i] = i - fleet_size + 1
+
+        # make first column an index column
+        for i in range(1, len(converted)):
+            if i <= fleet_size:
+                converted[i][0] = 1
+            else:
+                converted[i][0] = i - fleet_size + 1
+
+        # seting depot distance for columns
+        for i in range(1, fleet_size + 1):
+            matrix_item = 1
+            for j in range(fleet_size + 1, len(converted[i])):
+                converted[i][j] = float(matrix[0][matrix_item])
+                matrix_item += 1
+
+        # seting depot distance for rows
+        for j in range(1, fleet_size + 1):
+            matrix_row = 1
+            for i in range(fleet_size + 1, len(converted[i])):
+                converted[i][j] = float(matrix[matrix_row][0])
+                matrix_row += 1
+
+        # copying the rest of the values
+        matrix_row = 1
+        for i in range(fleet_size + 1, len(converted)):
+            matrix_item = 1
+            for j in range(fleet_size + 1, len(converted[i])):
+                converted[i][j] = float(matrix[matrix_row][matrix_item])
+                if i == j:
+                    converted[i][j] = float("inf")
+                matrix_item += 1
+            matrix_row += 1
+
+        return np.array(converted)
--- tests/tests_branchnbound.py
+++ tests/tests_branchnbound.py
@@ -19,19 +19,35 @@ class TestBnBPartialSolution(unittest.TestCase):
             [3, 6, 8, 0, 1],
             [4, 7, 9, 1, 0]
         ]
-        converted_matrix = bnb.convert(entry_matrix, fleet_size=2)
-        master = [
-            [float("inf"), 1, 1, 2, 3, 4, 5],
-            [1, float("inf"), float("inf"), 1.0, 2.0, 3.0, 4.0],
-            [1, float("inf"), float("inf"), 1.0, 2.0, 3.0, 4.0],
-            [2, 1.0, 1.0, float("inf"), 5.0, 6.0, 7.0],
-            [3, 2.0, 2.0, 5.0, float("inf"), 8.0, 9.0],
-            [4, 3.0, 3.0, 6.0, 8.0, float("inf"), 1.0],
-            [5, 4.0, 4.0, 7.0, 9.0, 1.0, float("inf")]
+        converted_matrix = bnb.BnBPartialSolution.convert(entry_matrix, fleet_size=2)
+        master1 = [
+            [float("inf"), 1., 1., 2., 3., 4., 5.],
+            [1., float("inf"), float("inf"), 1.0, 2.0, 3.0, 4.0],
+            [1., float("inf"), float("inf"), 1.0, 2.0, 3.0, 4.0],
+            [2., 1.0, 1.0, float("inf"), 5.0, 6.0, 7.0],
+            [3., 2.0, 2.0, 5.0, float("inf"), 8.0, 9.0],
+            [4., 3.0, 3.0, 6.0, 8.0, float("inf"), 1.0],
+            [5., 4.0, 4.0, 7.0, 9.0, 1.0, float("inf")]
         ]
 
-        self.assertEqual(master, converted_matrix)
+        for master_row, converted_row in zip(master1, converted_matrix):
+            for master_val, converted_val in zip(master_row, converted_row):
+                self.assertEqual(master_val, converted_val)
 
+        master2 = [
+            [float("inf"), 1., 1., 1., 2., 3., 4., 5.],
+            [1., float("inf"), float("inf"), float("inf"), 1.0, 2.0, 3.0, 4.0],
+            [1., float("inf"), float("inf"), float("inf"), 1.0, 2.0, 3.0, 4.0],
+            [1., float("inf"), float("inf"), float("inf"), 1.0, 2.0, 3.0, 4.0],
+            [2., 1.0, 1.0, 1.0, float("inf"), 5.0, 6.0, 7.0],
+            [3., 2.0, 2.0, 2.0, 5.0, float("inf"), 8.0, 9.0],
+            [4., 3.0, 3.0, 3.0, 6.0, 8.0, float("inf"), 1.0],
+            [5., 4.0, 4.0, 4.0, 7.0, 9.0, 1.0, float("inf")]
+        ]
+        converted_matrix2 = bnb.BnBPartialSolution.convert(entry_matrix, fleet_size=3)
+        for master_row, converted_row in zip(master2, converted_matrix2):
+            for master_val, converted_val in zip(master_row, converted_row):
+                self.assertEqual(master_val, converted_val)
 
 if __name__ == "__main__":
     unittest.main()
