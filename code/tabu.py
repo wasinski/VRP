55811f491243df08de408f1fba61a552f4105da6
Antash <4nt45h@gmail.com>
2015-06-17 02:49:09 +0200

    first tabu sketch

--- code/tabu.py
+++ code/tabu.py
@@ -1,5 +1,52 @@
+import copy
+
+DEPOT = 1
+
+
 class TabuSearch(object):
-    pass
+
+    def __init__(self, instance):
+        self.tabu = []
+        self.instance = instance
+        self.current_best = copy.deepcopy(instance)  # copy do zrobienia
+        self.best_value
+        self.iterations
+
+    def run(self):
+        while self.iterations > 0:
+            pass
+            self.iterations -= 1
+
+    def optimize_internal(self):
+        for vehicle in self.instance.fleet:
+            vehicle.route
+            pass  # tak tak...
+
+    def optimize_intra(self):
+        for vehicle in self.instance.fleet:
+            vehicle.route
+            pass  # tak tak
+
+    def swap_2opt(self, route, i, k):
+        new_route = []
+        pass
+
+    def swap_intra(self, source_route, dest_route, node, position):
+        pass
+
+    def choose_edge(self):
+        pass
+
+    def eval(self):
+        check_feasibility()
+        calculate_value()
+
+    def check_feasibility(self):
+        pass
+
+    def calculate_value(self):
+        pass
+
 
 
 class ParallelTS(object):
